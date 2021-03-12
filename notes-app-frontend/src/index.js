e81da97f60aa3a8a777355decc9a06821c8d1640
Jade McDaniels <jademcdaniels81@gmail.com>
2021-03-11 23:58:10 -0500

    created at, greetings, top

--- notes-app-frontend/src/index.js
+++ notes-app-frontend/src/index.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 const BASE_URL = "http://localhost:3000";
 const USERS_URL = `${BASE_URL}/users/`;
 const NOTES_URL = `${BASE_URL}/notes/`;
@@ -191,4 +196,25 @@ if (localStorage.id) {
         login(user);
       }
     });
+}
+
+// SCROLL TO TOP BOTTON
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
